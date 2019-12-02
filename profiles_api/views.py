b671edafb4bcab4e216e9c393b8b51ee0ce707d8
adminstratserver <admin@optionnalytics.com>
2019-12-02 16:46:46 +0800

    new changes API view - PUT, PATCH, DELETE

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
-# Create your views here.
+def HelloAPI(request):
+    return HttpResponse("Hello World!")
+
+class HelloAPIView(APIView):
+    def get(self, request):
+        init1 = [
+            'data1',
+            'data2',
+            'data3'
+                ]
+        init2 = [
+            'data4',
+            'data5',
+            'data6'
+        ]
+        return Response({'key1':init1, 'key2':init2})
+
+    serializer_class = serializers.HelloSerializer
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                # status=status.HTTP_400_BAD_REQUEST
+                status=400
+            )
+
+
+    def put(self, request, pk=None):
+        """Create a put message """
+        return Response({'Method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Create a patch message """
+        return Response({'Method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Create a delete message """
+        return Response({'Method': 'DELETE'})
--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api/', include('profiles_api.urls')),
     path('admin/', admin.site.urls),
 ]
